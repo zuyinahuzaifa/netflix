d4d8ec726a5f5ba2fb99411cae2ad6d69f1bf883
zuyinahuzaifa <zuyinasiddique11@gmail.com>
2025-02-04 09:02:39 +0530

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,25 @@
+<?php
+include 'config.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+    $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $username, $email, $password);
+
+    if ($stmt->execute()) {
+        echo "Signup successful! <a href='login.php'>Login here</a>";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+}
+?>
+
+<form method="POST">
+    <input type="text" name="username" placeholder="Username" required>
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Sign Up</button>
+</form>
